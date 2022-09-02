c5f21629103ce20e88c71c8efb4fc2028afae7ef
A-Prudnikova <a.prudnikova@gmail.com>
2022-09-02 16:04:21 +0300

    responce logs added

--- utils/session.py
+++ utils/session.py
@@ -10,17 +10,24 @@ class BaseSession(Session):
         self.base_url = kwargs.pop('base_url')
         super().__init__()
 
+    @allure.step('{method} {url}')
     def request(self, method, url, **kwargs):
-        with allure.step(f'{method} {url}'):
-            response = super().request(method, url=f'{self.base_url}{url}', **kwargs)
-            message = curlify.to_curl(response.request)
-            logging.info(f'{response.json()} {message}')
-            allure.attach(
-                body=message.encode('utf8'),
-                name=f'Request {method} {response.json()}',
-                attachment_type=allure.attachment_type.TEXT,
-                extension='txt'
-            )
+        response = super().request(method, url=f'{self.base_url}{url}', **kwargs)
+        message = curlify.to_curl(response.request)
+        logging.info(f'cURL: {message}\n')
+        logging.info(f'Response: {response.json()}\n')
+        allure.attach(
+            body=message.encode('utf8'),
+            name=f'Request {method}',
+            attachment_type=allure.attachment_type.TEXT,
+            extension='txt'
+        )
+        allure.attach(
+            body=str(response.json()).encode('utf-8'),
+            name='Response',
+            attachment_type=allure.attachment_type.TEXT,
+            extension='txt'
+        )
         return response
 
 
